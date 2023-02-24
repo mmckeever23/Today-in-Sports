86fc23728720fa774c4c32aaf4750e01ef04ba9e
Matt McKeever <mmckeever23@gmail.com>
2023-02-24 14:09:42 -0600

    Navbar updates

--- mlbscript.js
+++ mlbscript.js
@@ -2,16 +2,6 @@ window.addEventListener("load", function() {
     document.getElementById("date").innerHTML = moment().format('dddd, MMMM Do, Y');
     document.getElementById("time").innerHTML = moment().format('LT');
     document.getElementById("year").innerHTML = moment().format('YYYY');
-
-    var second = 0;
-    function pad ( value ) { return value > 9 ? value : value; }
-    setInterval( function(){
-        document.getElementById("seconds").innerHTML=pad(++second%61);
-    }, 1000);
-    if (second == 0) {
-        document.getElementById("seconds").innerHTML=0;
-    }
-
     let gamesList1 = document.getElementById('games-list1')
     let gamesList2 = document.getElementById('games-list2')
     let gamesList3 = document.getElementById('games-list3')
